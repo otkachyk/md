2aac24e1867a7fed7367603f5b0b091f4df7278e
Alexandr Tkachyk <adev.tkachyk@gmail.com>
2018-07-15 21:56:01 +0300

    create form for patients

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,20 @@
 class Card < ApplicationRecord
+  # t.integer "patient_id"
+  # t.string "diagnosis"
+  # t.string "complaints"
+  # t.string "previous_diseases"
+  # t.string "progressive_diseases"
+  # t.string "bite"
+  # t.string "dental_information"
+  # t.string "x_ray"
+  # t.string "color_vita"
+  # t.date "teaching_date"
+  # t.date "dental_hygiene_control_date"
+  # t.datetime "created_at", null: false
+  # t.datetime "updated_at", null: false
+  # t.index ["patient_id"], name: "index_cards_on_patient_id"
+
+
   belongs_to :patient
 
   has_many :notes
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,16 @@
 class Doctor < ApplicationRecord
+  # t.string "first_name"
+  # t.string "last_name"
+  # t.string "middle_name"
+  # t.string "gender"
+  # t.date "birthdate"
+  # t.integer "telephone"
+  # t.string "living_place"
+  # t.string "email"
+  # t.datetime "created_at", null: false
+  # t.datetime "updated_at", null: false
+
+
   has_many :notes
   has_many :cards, through: :notes
 
