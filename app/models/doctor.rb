04aa8eb3abad955d98ce96d38e8baf4e04309580
Alexandr Tkachyk <adev.tkachyk@gmail.com>
2018-07-15 20:37:12 +0300

    add appointment model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,7 @@
 class Doctor < ApplicationRecord
   has_many :notes
   has_many :cards, through: :notes
+
+  has_many :appointments
+  has_many :patients, through: :appointments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180715123324) do
+ActiveRecord::Schema.define(version: 20180715173525) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.integer "patient_id"
+    t.integer "doctor_id"
+    t.datetime "meeting_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
 
   create_table "cards", force: :cascade do |t|
     t.integer "patient_id"
